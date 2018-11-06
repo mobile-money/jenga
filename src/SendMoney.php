ffb45f83ae3e86a366af65a30847231a8c171a44
Mauko Maunde <maukoese@gmail.com>
2018-11-06 15:23:24 +0300

    Create SendMoney.php

---
+++ src/SendMoney.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Jenga For WooCommerce
+ * @author Osen Concepts < hi@osen.co.ke >
+ * @version 1.10
+ */
+namespace Osen;
+
+/**
+ * 
+ */
+class SendMoney extends Jenga
+{
+}
